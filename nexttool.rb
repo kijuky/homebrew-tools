ada1c36d119e434eaa5e922d4db349e4059ab51f
YASUE Kizuki <ikuzik@kyudai.jp>
2018-06-16 00:02:50 +0900

    [Update] nexttool (1.2.1.r5)

--- nexttool.rb
+++ nexttool.rb
@@ -1,13 +1,11 @@
-require "formula"
-
 HOMEBREW_NEXTTOOL_VERSION="1.2.1.r5"
 class Nexttool < Formula
   homepage "http://bricxcc.sourceforge.net/"
-  url "https://github.com/kijuky/NeXTTool/archive/#{HOMEBREW_NEXTTOOL_VERSION}.tar.gz"
-  sha256 "19a3734f27c6fa314c2eb5317c251b6c55037263e67a1f25c8c2d0cd4db4c0c7"
+  url "http://bricxcc.sourceforge.net/nexttool_osx.zip"
+  sha256 "7d455fe10191f06cbccd69075449041663c7e98fed917f49d18f24ca1f87f6d6"
   version HOMEBREW_NEXTTOOL_VERSION
 
   def install
-    bin.install Dir['bin/*']
+    bin.install Dir["nexttool"]
   end
 end
