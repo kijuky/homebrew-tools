f2b4340ae22881049b9876d2039eaa9bede2e9a4
kijuky <kijuky@users.noreply.github.com>
2022-09-23 12:49:48 +0000

    [create-pull-request] automated change

--- line-notify.rb
+++ line-notify.rb
@@ -1,11 +1,11 @@
 class LineNotify < Formula
   desc "LINE Notify CLI."
   homepage "https://github.com/kijuky/line-notify"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
-  url "https://github.com/kijuky/line-notify/releases/download/0.1.3/line-notify.tar.gz"
-  sha256 "d298a81b683db6f0d5e5584e492ce04421fdeb673a84b6727a5bf33ac6f4673b"
+  url "https://github.com/kijuky/line-notify/releases/download/0.1.4/line-notify.tar.gz"
+  sha256 "0ae5a12f10f9513cb2a58d44be3cd69d027a155abc10b10f86607cde2e2e93f7"
 
   def install
     bin.install "line-notify"
